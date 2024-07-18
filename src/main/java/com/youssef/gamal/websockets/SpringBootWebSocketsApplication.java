90fdda646a79365ec95f6e5752d6a6d96768b075
Youssef Gamal <youssef.gamal@intercom.com.eg>
2024-07-18 13:23:34 +0300

    Initalized Project

---
+++ src/main/java/com/youssef/gamal/websockets/SpringBootWebSocketsApplication.java
@@ -0,0 +1,13 @@
+package com.youssef.gamal.websockets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebSocketsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebSocketsApplication.class, args);
+    }
+
+}
